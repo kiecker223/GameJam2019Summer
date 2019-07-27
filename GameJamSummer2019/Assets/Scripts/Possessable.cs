bb9127946c0ba965cf842893579eda676c35fcce
Justin <321rooster@gmail.com>
2019-07-27 10:50:33 -0700

    Added possession mechanics to the game

---
+++ GameJamSummer2019/Assets/Scripts/Possessable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Possessable : MonoBehaviour
+{
+
+    public string state;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
