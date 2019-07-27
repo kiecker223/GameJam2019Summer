775aed04fff4246cd1b4639bc0d1067f08619283
Alex Kiecker <kiecker223@gmail.com>
2019-07-26 21:41:20 -0500

    Initial commit

---
+++ GameJamSummer2019/Assets/Scripts/InputManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+	public static InputManager Instance { get; private set; }
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	// TODO: Player controller support
+	public float GetHorizontalAxisLeftStick_Player1()
+	{
+		return Input.GetAxis("Joy_1_LH");
+	}
+
+	public float GetVerticalAxisRightStick_Player1()
+	{
+		return Input.GetAxis("Joy_1_LV");
+	}
+
+	void Update()
+	{
+
+	}
+}
---
+++ GameJamSummer2019/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private Rigidbody2D m_Rb;
+
+	public float characterSpeedHorizontal;
+
+    void Start()
+    {
+		m_Rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+		float left = InputManager.Instance.GetHorizontalAxisLeftStick_Player1() * characterSpeedHorizontal;
+		m_Rb.velocity = new Vector3(left, 0, 0);
+    }
+}
